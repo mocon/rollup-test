08f2c0d75d91a2c9f1df17be488424c73d05b4aa
Myles O'Connor <myles@gumgum.com>
2016-11-18 10:09:09 -0800

    Updated Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 // Rollup plugins
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
     entry: 'src/scripts/main.js',
@@ -8,6 +12,12 @@ export default {
     format: 'iife',
     sourceMap: 'inline',
     plugins: [
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true
+        }),
+        commonjs(),
         eslint({
             exclude: [
                 'src/styles/**'
@@ -15,6 +25,10 @@ export default {
         }),
         babel({
             exclude: 'node_modules/**'
-        })
+        }),
+        replace({
+            ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
+        (process.env.NODE_ENV === 'production' && uglify())
     ]
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('Testing');
+import debug from 'debug';
+
+const log = debug('app:log');
+
+// Disable logging in production
+if (ENV !== 'production') {
+    debug.enable('*');
+    log('Logging is enabled!');
+} else {
+    debug.disable();
+}
